f9d5742d476f6589a5bf4457b8ab0dfd56ef2c25
Hex <hex@codeigniter.org.cn>
2015-08-27 14:56:37 +0800

    update

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -6,4 +6,4 @@ git config user.name "Travis CI"
 git config user.email "hex@codeigniter.org.cn"
 git add .
 git commit -m "Deploy to GitHub Pages"
-git push --force "https://${GH_TOKEN}@${GH_REF}" master:gh-pages
+git push --force "https://${GH_TOKEN}@github.com/CodeIgniter-Chinese/codeigniter-user-guide.git" master:gh-pages
