e2a59229011ed9ab90fd7f8291c0ebbb08a6b4d7
Hex <hex@codeigniter.org.cn>
2015-08-27 18:29:10 +0800

    update

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -3,8 +3,9 @@
 git config --global user.name "Travis-CI"
 git config --global user.email "support@codeigniter.org.cn"
 git clone --branch=gh-pages "https://${GH_TOKEN}@${GH_REF}" gh-pages
+cd ./build/html
+zip -r ./codeigniter_user_guide.zip ./
 cd gh-pages
-zip -r ./codeigniter_user_guide.zip ../build/html/
 cp -Rf ../build/html/* .
 git add -f .
 git commit -m "Deploy to GitHub Pages"
