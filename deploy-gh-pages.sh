bd9d32e518880f7ecc0f8f3c1f364e33d859c67b
Hex <hex@codeigniter.org.cn>
2015-08-27 17:15:03 +0800

    update

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-git config --global user.name "Hex"
-git config --global user.email "hex@codeigniter.org.cn"
+git config --global user.name "Travis-CI"
+git config --global user.email "support@codeigniter.org.cn"
 git clone --branch=gh-pages "https://${GH_TOKEN}@github.com/CodeIgniter-Chinese/codeigniter-user-guide.git" gh-pages
 cd gh-pages
 cp -Rf ../build/html/* .
