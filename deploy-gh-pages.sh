3fbb2711d82f629f5fef80ebf622d364b10e6db2
Hex <hex@codeigniter.org.cn>
2015-08-27 17:06:24 +0800

    update

--- deploy-gh-pages.sh
+++ deploy-gh-pages.sh
@@ -2,9 +2,9 @@
 
 git config --global user.name "CodeIgniter"
 git config --global user.email "hex@codeigniter.org.cn"
-git clone --branch=gh-pages https://${GH_TOKEN}@github.com/CodeIgniter-Chinese/codeigniter-user-guide.git gh-pages
+git clone --branch=gh-pages "https://${GH_TOKEN}@github.com/CodeIgniter-Chinese/codeigniter-user-guide.git" gh-pages
 cd gh-pages
-cp -Rf ./build/html/* .
+cp -Rf ../build/html/* .
 git add -f .
 git commit -m "Deploy to GitHub Pages"
 git push -fq origin gh-pages
